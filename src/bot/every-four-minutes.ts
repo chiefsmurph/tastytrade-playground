e3570dba44243690dc490d2c298f4f577faeaccc
John Murphy <chiefsmurph@gmail.com>
2026-06-20 20:26:03 -0700

    fix

--- src/bot/every-four-minutes.ts
+++ src/bot/every-four-minutes.ts
@@ -1,4 +1,5 @@
 import tastytradeApi from "../core/tastytrade-client";
+import { CurrentPosition } from "../core/types";
 
 export default async function everyFourMinutes() {
   const accounts =
@@ -7,19 +8,23 @@ export default async function everyFourMinutes() {
     (item: any) => item.account["account-number"],
   );
   const accountNumber = extractedAccountNumbers[0];
-  console.log({ accounts, extractedAccountNumbers, accountNumber});
+  console.log({ accounts, extractedAccountNumbers, accountNumber });
   const accountBalances =
-    await tastytradeApi.balancesAndPositionsService.getAccountBalanceValues(accountNumber);
-  console.log("Current account balances:", JSON.stringify(accountBalances, null, 2));
+    await tastytradeApi.balancesAndPositionsService.getAccountBalanceValues(
+      accountNumber,
+    );
+  console.log(
+    "Current account balances:",
+    JSON.stringify(accountBalances, null, 2),
+  );
 
   // implement cancel all open orders before getting current buying power to ensure we have the most accurate buying power available for new orders
 
-  const buyingPower = Number(accountBalances['derivative-buying-power']);
+  const buyingPower = Number(accountBalances["derivative-buying-power"]);
   console.log("Current buying power:", buyingPower);
 
-
-    const currentPositions: CurrentPosition[] =
-      await tastytradeApi.balancesAndPositionsService.getPositionsList(
-        extractedAccountNumbers[0],
-      );
+  const currentPositions: CurrentPosition[] =
+    await tastytradeApi.balancesAndPositionsService.getPositionsList(
+      extractedAccountNumbers[0],
+    );
 }
